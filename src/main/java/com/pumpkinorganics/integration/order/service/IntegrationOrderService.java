6935e84642149845ca7c49fef2255f5e57833744
Mark Hoja <mark.hoja@web.de>
2018-01-31 16:59:02 +0100

    _m_o,O_m_

--- src/main/java/com/pumpkinorganics/integration/order/service/IntegrationOrderService.java
+++ src/main/java/com/pumpkinorganics/integration/order/service/IntegrationOrderService.java
@@ -1,15 +1,25 @@
 package com.pumpkinorganics.integration.order.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.bakerbeach.market.core.service.order.dao.OrderDaoException;
 import com.bakerbeach.market.core.service.order.service.OrderServiceImpl;
 import com.bakerbeach.market.order.api.model.OrderList;
 
-public class IntegrationOrderService extends OrderServiceImpl{
-	
-	public OrderList findByOrderPeriod(Date startDate, Date endDate) {
-		
-		return null;
-	}
+public class IntegrationOrderService extends OrderServiceImpl {
 
+	public OrderList findByOrderPeriod(Date startDate, Date endDate) {
+		Map<String, Object> filters = new HashMap<String, Object>();
+		filters.put("createdAt >=", startDate);
+		filters.put("createdAt <=", endDate);
+		OrderList orderList;
+		try {
+			orderList = orderDaos.get("PUMPKIN_DE").findByFilters(filters, null, null, null);
+			return orderList;
+		} catch (OrderDaoException e) {
+			return null;
+		}
+	}
 }
--- src/test/java/com/pumpkinorganics/integration/order/service/IntegrationOrderServiceTest.java
+++ src/test/java/com/pumpkinorganics/integration/order/service/IntegrationOrderServiceTest.java
@@ -1,7 +1,6 @@
 package com.pumpkinorganics.integration.order.service;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import org.junit.Test;
@@ -23,10 +22,22 @@ public class IntegrationOrderServiceTest {
 	
 	@Test
 	public void listOrders() throws OrderServiceException {
-		GregorianCalendar sart = new GregorianCalendar();
-		sart.add(Calendar.MONTH, -1);
-		GregorianCalendar stopDate = new GregorianCalendar(); 
-		integrationOrderService.findByOrderPeriod(null, null);
+		GregorianCalendar start = new GregorianCalendar();
+		//start.add(Calendar.MONTH, -1);
+		start.set(Calendar.DAY_OF_MONTH, 1);
+		start.set(Calendar.HOUR_OF_DAY, 0);
+		start.set(Calendar.MINUTE, 0);
+		start.set(Calendar.SECOND, 0);
+		start.set(Calendar.MILLISECOND, 0);
+		GregorianCalendar stop = new GregorianCalendar(); 
+		stop.set(Calendar.YEAR, start.get(Calendar.YEAR));
+		stop.set(Calendar.MONTH, start.get(Calendar.MONTH));
+		stop.set(Calendar.DAY_OF_MONTH, start.getActualMaximum(Calendar.DAY_OF_MONTH));
+		stop.set(Calendar.HOUR_OF_DAY, 23);
+		stop.set(Calendar.MINUTE, 59);
+		stop.set(Calendar.SECOND, 59);
+		stop.set(Calendar.MILLISECOND, 999);
+		integrationOrderService.findByOrderPeriod(start.getTime(), stop.getTime());
 	}
 
 }
