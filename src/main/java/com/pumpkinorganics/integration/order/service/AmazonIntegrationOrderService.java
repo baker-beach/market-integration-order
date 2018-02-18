29e73d5ce3fa94826dc12d851e7d51bb21e61016
Mark Hoja <mark.hoja@web.de>
2018-02-18 23:58:23 +0100

    clen up

---
+++ src/main/java/com/pumpkinorganics/integration/order/service/AmazonIntegrationOrderService.java
@@ -0,0 +1,10 @@
+package com.pumpkinorganics.integration.order.service;
+
+public class AmazonIntegrationOrderService extends IntegrationOrderService{
+	
+	public static final String AMAZON_CUSTOMER_ID = "1247";
+	public static final String AMAZON_ORDER_ID_KEY = "amazonOrderId";
+	public static final String AMAZON_PAYMENT_CODE = "AMAZON";
+	public static final String AMAZON_SHOP_CODE = "PUMPKIN_AMAZON";
+
+}
--- src/main/java/com/pumpkinorganics/integration/order/service/IntegrationOrderService.java
+++ src/main/java/com/pumpkinorganics/integration/order/service/IntegrationOrderService.java
@@ -10,33 +10,27 @@ import com.bakerbeach.market.order.api.model.OrderList;
 
 public class IntegrationOrderService extends OrderServiceImpl {
 
-	public OrderList findByOrderPeriod(Date startDate, Date endDate) {
+	public OrderList findByOrderPeriod(String shopCode, Date startDate, Date endDate) {
 		Map<String, Object> filters = new HashMap<String, Object>();
 		filters.put("createdAt >=", startDate);
 		filters.put("createdAt <=", endDate);
-		String[] status = {"sent","logistic"};
-		filters.put("status in",status);
-		filters.put("total.gross !=","0.00");
 		OrderList orderList;
 		try {
-			orderList = orderDaos.get("PUMPKIN_DE").findByFilters(filters, null, null, null, false);
+			orderList = orderDaos.get(shopCode).findByFilters(filters, null, null, null, false);
 			return orderList;
 		} catch (OrderDaoException e) {
 			return null;
 		}
 	}
 	
-	public OrderList findByInvoicePeriod(Date startDate, Date endDate,String paymentCode) {
+	public OrderList findByInvoicePeriod(String shopCode,Date startDate, Date endDate,String paymentCode) {
 		Map<String, Object> filters = new HashMap<String, Object>();
 		filters.put("invoices.invoiceDate >=", startDate);
 		filters.put("invoices.invoiceDate <=", endDate);
-		String[] status = {"sent","logistic"};
-		filters.put("status in",status);
-		filters.put("total.gross !=","0.00");
 		filters.put("paymentCode =",paymentCode);
 		OrderList orderList;
 		try {
-			orderList = orderDaos.get("PUMPKIN_DE").findByFilters(filters, null, null, null, false);
+			orderList = orderDaos.get(shopCode).findByFilters(filters, null, null, null, false);
 			return orderList;
 		} catch (OrderDaoException e) {
 			return null;
